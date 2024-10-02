c2333bf19c2cbf673c740026d49e5c6add86ce14
Raheemah Adegboye <105223500+RaheemahAde@users.noreply.github.com>
2024-10-02 20:10:05 +0100

    refactor: improve test structure

--- tests/swag-labs.spec.ts
+++ tests/swag-labs.spec.ts
@@ -1,26 +1,17 @@
-import { test, expect } from '@playwright/test';
+import { test, expect, Page } from '@playwright/test';
 import {
   addToCartByProductName,
   removeItemFromCart,
 } from '../helpers/cartHelpers';
-import { completeCheckout } from '../helpers/checkoutHelpers';
+import { addCheckoutInfo } from '../helpers/checkoutHelper';
+import { login } from '../helpers/loginHelper';
 import * as constants from '../config/constants';
-
-let itemTotal;
-let taxValue;
-let priceTotal;
 
 test.describe('Swag Labs - Cart and Checkout Functionality', () => {
   test.beforeEach('should login', async ({ page }) => {
-    await page.goto(constants.url);
-    await expect(page).toHaveTitle(constants.expectedPageTitle);
-
-    await page.getByPlaceholder('Username').fill(constants.username);
-    await page.getByPlaceholder('Password').fill(constants.password);
-    await page.getByText('Login').click();
-    await expect(page).toHaveURL(constants.productUrl);
+    await login(page);
   });
-  test('add items to cart, checkout and make payment', async ({ page }) => {
+  test('should add items to cart and checkout', async ({ page }) => {
     await addToCartByProductName(page, constants.product1);
     await addToCartByProductName(page, constants.product2);
 
@@ -45,54 +36,56 @@ test.describe('Swag Labs - Cart and Checkout Functionality', () => {
 
     await removeItemFromCart(page, constants.backPackId);
 
-    await expect(backpackItem).not.toBeVisible();
+    await expect(backpackItem).not.toBeAttached();
     await expect(bikeLightItem).toBeVisible();
 
     await page.getByRole('button', { name: 'Checkout' }).click();
 
-    const checkoutPageTitle = page.getByText('Checkout: Your Information');
-    await expect(checkoutPageTitle).toBeVisible();
+    const checkoutPageStepOneTitle = page.getByText(
+      'Checkout: Your Information'
+    );
+    await expect(checkoutPageStepOneTitle).toBeVisible();
 
-    await completeCheckout(
+    await addCheckoutInfo(
       page,
       constants.firstName,
       constants.lastName,
       constants.postcode
     );
     await page.getByTestId('continue').click();
-    //await expect(page).to
+
+    const checkoutPageStepTwoTitle = page.getByTestId('title');
+    await expect(checkoutPageStepTwoTitle).toBeVisible();
+    await expect(checkoutPageStepTwoTitle).toContainText('Checkout');
 
     // Locate the item price
-    const itemTotalText = await page
-      .getByTestId('subtotal-label')
-      .textContent();
+    async function getPriceFromText(page: Page, textId: string) {
+      const text = await page.getByTestId(textId).textContent();
 
-    if (itemTotalText) {
-      itemTotal = parseFloat(itemTotalText.replace(/[^\d.]/g, '').trim());
-    } else {
-      throw new Error('Price not found!');
+      if (text === null) {
+        throw new Error(`Text not found for testId: ${textId}`);
+      }
+      // Return the parsed price if the text exists
+      return parseFloat(text.replace(/[^\d.]/g, '').trim());
     }
 
-    // Locate the tax value
-    const taxText = await page.getByTestId('tax-label').textContent();
-    if (taxText) {
-      taxValue = parseFloat(taxText.replace(/[^\d.]/g, '').trim());
-    } else {
-      throw new Error('Tax Value not found!');
-    }
-    // Locate the price total
-    const totalText = await page.getByTestId('total-label').textContent();
-    if (totalText) {
-      priceTotal = parseFloat(totalText.replace(/[^\d.]/g, '').trim());
-    } else {
-      throw new Error('Price Total not found');
-    }
+    const itemTotal = getPriceFromText(page, 'subtotal-label');
+    const taxValue = await getPriceFromText(page, 'tax-label');
+    const priceTotal = await getPriceFromText(page, 'total-label');
 
-    const expectedTotal = (itemTotal ?? 0) + (taxValue ?? 0);
-    // Round both totals to 2 decimal places
-    const roundedExpectedTotal = Math.round(expectedTotal * 100) / 100;
-    const roundedPriceTotal = Math.round((priceTotal ?? 0) * 100) / 100;
+    const roundToTwoDecimals = (value: number) => Math.round(value * 100) / 100;
 
-    expect(roundedExpectedTotal).toEqual(roundedPriceTotal);
+    const expectedTotal = (await (itemTotal ?? 0)) + (taxValue ?? 0);
+    expect(roundToTwoDecimals(expectedTotal)).toEqual(
+      roundToTwoDecimals(priceTotal ?? 0)
+    );
+
+    await page.getByRole('button', { name: 'Finish' }).click();
+    const checkoutComplete = page.getByTestId('complete-header');
+    await expect(checkoutComplete).toBeVisible();
+  });
+  test.afterEach(async ({ page }) => {
+    await page.getByRole('button', { name: 'Open Menu' }).click();
+    await page.getByRole('link', { name: 'Logout' }).click();
   });
 });
